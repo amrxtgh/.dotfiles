425bf9122da7536b61473988ee53ed304af08ada
amrxtgh <ghimireamrit619@gmail.com>
2025-08-30 16:08:42 +0545

    smylink

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,6 +11,7 @@ DOTFILES="$HOME/.dotfiles"
 
 # Ensure config directory exists
 mkdir -p "$HOME/.config"
+mkdir -p ~/.ssh
 
 rm -rf ~/.gitconfig
 ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
@@ -20,6 +21,8 @@ rm -rf ~/.config/nvim
 # Neovim symlink
 ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
 
+ln -sf ~/.dotfiles/ssh/config ~/.ssh/config
+ln -sf ~/.dotfiles/ssh/id_rsa_github ~/.ssh/id_rsa_github
 
 echo "All symlinks created successfully!"
 
