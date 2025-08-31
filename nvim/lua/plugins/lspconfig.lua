d72d654660c67fe80f9f56fba1c90ccbf58928e7
amrxtgh <ghimireamrit619@gmail.com>
2025-08-31 14:27:57 +0545

    add java lsp temp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -22,11 +22,37 @@ return {
       })
 
       local lspconfig = require("lspconfig")
-      local servers = { "lua_ls", "vimls", "pyright", "ts_ls", "html", "cssls", "bashls", "jsonls", "clangd" }
+      local servers = { "lua_ls", "vimls", "pyright", "ts_ls", "html", "cssls", "bashls", "jsonls", "clangd", "jdtls"}
 
       for _, server in ipairs(servers) do
         lspconfig[server].setup({})
-      end
+      local jdtls = require("jdtls")
+      local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+      local jdtls_jar = vim.fn.glob("~/.local/share/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")
+      local jdtls_config = "~/.local/share/jdtls/config_linux"
+
+      local java_config = {
+        cmd = {
+          "java",
+          "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+          "-Dosgi.bundles.defaultStartLevel=4",
+          "-Declipse.product=org.eclipse.jdt.ls.core.product",
+          "-noverify",
+          "-Xmx1G",
+          "-jar", jdtls_jar,
+          "-configuration", jdtls_config,
+          "-data", workspace_dir,
+        },
+        root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
+      }
+
+      -- Auto-attach JDTLS only for Java files
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "java",
+        callback = function()
+          jdtls.start_or_attach(java_config)
+        end,
+      })
     end,
   },
 }
