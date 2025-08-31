34a6b7e43dfd362b23a9ba58805f39ab6a7e4e05
amrxtgh <ghimireamrit619@gmail.com>
2025-08-31 21:34:40 +0545

    Modify setup.sh and add alacritty config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,7 @@ rm -rf ~/.gitconfig
 rm -rf ~/.config/nvim
 rm -rf ~/.ssh/id_rsa_github
 rm -rf ~/.ssh/config
+rm -rf ~/.config/alacritty/alacritty.toml
 
 mkdir -p ~/.config
 mkdir -p ~/.ssh
@@ -13,7 +14,7 @@ cp ~/.dotfiles/git/.gitconfig ~/.gitconfig
 cp -r ~/.dotfiles/nvim/ ~/.config/nvim/
 cp ~/.dotfiles/ssh/config ~/.ssh/config
 cp ~/.dotfiles/ssh/id_rsa_github ~/.ssh/id_rsa_github
-
+cp ~/.dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 chmod 600 ~/.ssh/id_rsa_github
 
 echo "All symlinks created successfully!"
