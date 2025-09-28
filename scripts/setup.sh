95bebb4a80434ceaab415ac31883671060fe7721
amrxtgh69 <152877271+amrxtgh69@users.noreply.github.com>
2025-09-28 10:39:01 +0545

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-sudo pacman -Syu --noconfirm ripgrep fzf neovim git
+sudo pacman -Syu --noconfirm ripgrep fzf neovim git ttf-cascadia-mono-nerd
 
 rm -rf ~/.gitconfig
 rm -rf ~/.config/nvim
