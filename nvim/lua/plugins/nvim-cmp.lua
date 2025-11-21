492b805c76feb3668f5193b8d214d00f5cf832ef
amrxtgh <ghimireamrit619@gmail.com>
2025-11-21 12:24:38 +0545

    fix

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,128 +1,89 @@
--- ============================================================================
--- NVIM-CMP CONFIGURATION
--- ============================================================================
--- Minimal autocompletion setup with arrow key navigation
-
 return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",     -- LSP completion source
-    "hrsh7th/cmp-buffer",       -- Buffer words source
-    "hrsh7th/cmp-path",         -- File path source
-    "L3MON4D3/LuaSnip",         -- Snippet engine
-    "saadparwaiz1/cmp_luasnip", -- Snippet source
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
   },
+
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
-    
+
     cmp.setup({
-      -- ======================================================================
-      -- SNIPPET ENGINE
-      -- ======================================================================
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
-      
-      -- ======================================================================
-      -- COMPLETION WINDOW (Minimal - No Borders, No Documentation Box)
-      -- ======================================================================
+
       window = {
         completion = {
           border = "none",
           winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
         },
-        documentation = cmp.config.disable, -- Disable documentation box
+        documentation = cmp.config.disable,
       },
-      
-      -- ======================================================================
-      -- KEY MAPPINGS
-      -- ======================================================================
+
       mapping = cmp.mapping.preset.insert({
-        -- Navigate menu items with Up/Down arrows
         ["<Up>"] = cmp.mapping.select_prev_item(),
         ["<Down>"] = cmp.mapping.select_next_item(),
-        
-        -- Alternative navigation with Ctrl+j/k
-        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-k>"] = cmp.mapping.select_prev_item(),
-        
-        -- Tab: Move to next item (don't auto-confirm)
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            fallback()
-          end
+          if cmp.visible() then cmp.select_next_item()
+          else fallback() end
         end, { "i", "s" }),
-        
-        -- Shift-Tab: Move to previous item
+
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            fallback()
-          end
+          if cmp.visible() then cmp.select_prev_item()
+          else fallback() end
         end, { "i", "s" }),
-        
-        -- Enter: Confirm selection
+
         ["<CR>"] = cmp.mapping.confirm({
           select = true,
           behavior = cmp.ConfirmBehavior.Replace,
         }),
-        
-        -- Scroll documentation
+
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        
-        -- Trigger completion manually
+
         ["<C-Space>"] = cmp.mapping.complete(),
-        
-        -- Close completion menu
         ["<C-e>"] = cmp.mapping.abort(),
       }),
-      
-      -- ======================================================================
-      -- COMPLETION SOURCES
-      -- ======================================================================
+
       sources = cmp.config.sources({
-        { name = "nvim_lsp", priority = 1000, keyword_length = 1 },
+        { name = "nvim_lsp", priority = 1000 },
         { name = "luasnip", priority = 750 },
-        { name = "buffer", priority = 500, keyword_length = 3 },
+        { name = "buffer", priority = 500 },
         { name = "path", priority = 250 },
       }),
-      
-      -- ======================================================================
-      -- FORMATTING
-      -- ======================================================================
+
       formatting = {
         fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          -- Source labels
-          vim_item.menu = ({
+        format = function(entry, item)
+          item.menu = ({
             nvim_lsp = "[LSP]",
             luasnip = "[Snip]",
-            buffer = "[Buf]",
-            path = "[Path]",
+            buffer   = "[Buf]",
+            path     = "[Path]",
           })[entry.source.name]
-          
-          return vim_item
+          return item
         end,
       },
-      
-      -- ======================================================================
-      -- BEHAVIOR
-      -- ======================================================================
+
       completion = {
         completeopt = "menu,menuone,noinsert",
       },
-      
+
       experimental = {
-        ghost_text = false, -- Disable for minimal look
+        ghost_text = false,
       },
     })
   end,
 }
+
