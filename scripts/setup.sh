f57bc05afd78b01d4b068e02074278e9f3aa51ed
amrxtgh <ghimireamrit619@gmail.com>
2026-02-03 23:35:26 +0545

    added: change scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,9 @@ echo "Starting dotfiles setup..."
 
 # Update and install packages
 echo "Installing packages..."
-sudo pacman -Syu --noconfirm ripgrep fzf neovim git ttf-cascadia-mono-nerd nodejs npm python python-pip curl go rustup wmctrl zoxide
+sudo pacman -Syu --noconfirm ripgrep fzf git ttf-cascadia-mono-nerd nodejs npm python python-pip curl go rustup wmctrl zoxide
+# neovim commented out - using NvChad instead
+# sudo pacman -S --noconfirm neovim
 
 # Install yay if not present
 if ! command -v yay &> /dev/null; then
@@ -22,6 +24,10 @@ rm -f ~/.zshrc
 #rm -rf ~/.config/nvim
 rm -rf ~/.config/kitty
 rm -rf ~/.config/alacritty
+rm -rf ~/.config/hypr
+rm -rf ~/.config/niri
+rm -rf ~/.config/waybar
+rm -rf ~/.config/vicinae
 rm -f ~/.ssh/id_rsa_github
 rm -f ~/.ssh/config
 
@@ -29,6 +35,10 @@ rm -f ~/.ssh/config
 echo "Creating directories..."
 mkdir -p ~/.config/kitty
 mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/hypr
+mkdir -p ~/.config/niri
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/vicinae
 mkdir -p ~/.ssh
 
 # Copy configurations
@@ -39,6 +49,10 @@ echo "Copying configurations..."
 [ -f ~/.dotfiles/ssh/id_rsa_github ] && cp ~/.dotfiles/ssh/id_rsa_github ~/.ssh/id_rsa_github
 [ -d ~/.dotfiles/alacritty ] && cp -r ~/.dotfiles/alacritty/* ~/.config/alacritty/
 [ -d ~/.dotfiles/kitty ] && cp -r ~/.dotfiles/kitty/* ~/.config/kitty/
+[ -d ~/.dotfiles/hypr ] && cp -r ~/.dotfiles/hypr/* ~/.config/hypr/
+[ -d ~/.dotfiles/niri ] && cp -r ~/.dotfiles/niri/* ~/.config/niri/
+[ -d ~/.dotfiles/waybar ] && cp -r ~/.dotfiles/waybar/* ~/.config/waybar/
+[ -d ~/.dotfiles/vicinae ] && cp -r ~/.dotfiles/vicinae/* ~/.config/vicinae/
 [ -f ~/.dotfiles/zsh/.zshrc ] && cp ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
 # Set SSH permissions
