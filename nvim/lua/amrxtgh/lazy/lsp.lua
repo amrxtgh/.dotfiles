264f691a6b365fe58ab887eb2469780d4bdc2a4c
amrxtgh <ghimireamrit619@gmail.com>
2026-06-27 22:12:15 +0545

    change lsp

--- nvim/lua/amrxtgh/lazy/lsp.lua
+++ nvim/lua/amrxtgh/lazy/lsp.lua
@@ -8,6 +8,9 @@ return {
 		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
 
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+
 		"j-hui/fidget.nvim",
 	},
 	config = function()
@@ -16,12 +19,32 @@ return {
 			ensure_installed = {
 				"lua_ls",
 				"rust_analyzer",
+				"clangd",
+				"pylsp",
 			},
 		})
 
 		require("fidget").setup({})
 		local cmp = require("cmp")
-		cmp.setup({})
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			mappings = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+			}),
+			source = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+			})
+		})
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		vim.lsp.config("lua_ls", {
@@ -37,14 +60,31 @@ return {
 		vim.lsp.config("rust-analyzer", {
 			capabilities = capabilities,
 		})
+		vim.lsp.config("clangd", {
+			capabilities = capabilities,
+		})
+		vim.lsp.config("pylsp", {
+			capabilities = capabilities,
+			settings = {
+				pylsp = {
+					plugins = {
+						pylint = { enabled = true },
+					},
+				},
+			},
+		})
 		vim.lsp.enable("lua_ls")
 		vim.lsp.enable("rust-analyzer")
+		vim.lsp.enable("clangd")
+		vim.lsp.enable("pylsp")
 
 		vim.diagnostic.config({
 			float = {
 				border = "rounded",
 				source = "always",
 				focusable = false,
+				header = "",
+				prefix = "",
 			},
 		})
 	end,
