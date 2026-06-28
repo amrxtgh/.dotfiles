89526e8ec6f2710c23c97ae6d3568fbfdde38bb3
amrxtgh <ghimireamrit619@gmail.com>
2026-06-28 11:09:03 +0545

    added: rust configuration

---
+++ nvim/lua/amrxtgh/lazy/rustacean.lua
@@ -0,0 +1,27 @@
+return {
+	"mrcjkb/rustaceanvim",
+	version = "^9",
+	ft = { "rust" },
+	init = function()
+		vim.g.rustaceanvim = {
+			tools = {
+				hover_actions = {
+					["auto_focus"] = true,
+				},
+			},
+			server = {
+				on_attach = function(client, bufnr) end,
+				default_settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+			},
+		}
+	end,
+}
