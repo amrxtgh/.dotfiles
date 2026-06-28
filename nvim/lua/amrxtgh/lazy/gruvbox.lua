551ad88c907fc5a134565d16e85f3c005a7ff72e
amrxtgh <ghimireamrit619@gmail.com>
2026-06-28 23:33:17 +0545

    Add fugitive configuration and update neovim settings

--- nvim/lua/amrxtgh/lazy/gruvbox.lua
+++ nvim/lua/amrxtgh/lazy/gruvbox.lua
@@ -5,8 +5,13 @@ return {
 	config = function()
 		require("gruvbox").setup({
 			transparent_mode = true,
-			contrast = "hard",
+			contrast = "soft",
 		})
 		vim.cmd.colorscheme("gruvbox")
 	end,
+	--"rose-pine/neovim",
+	--name = "rose-pine",
+	--config = function ()
+	--	vim.cmd("colorscheme rose-pine")
+	--end
 }
