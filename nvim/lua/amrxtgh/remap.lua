d7b7e75e29cbec4efd1d861fe44b6c1753130d8a
amrxtgh <ghimireamrit619@gmail.com>
2026-07-01 02:44:52 +0545

    update to use lazygit

--- nvim/lua/amrxtgh/remap.lua
+++ nvim/lua/amrxtgh/remap.lua
@@ -7,3 +7,17 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch)
 vim.keymap.set("n", "<leader>T", vim.cmd.term)
+vim.keymap.set("n", "<leader>G", function()
+  vim.cmd.terminal("lazygit")
+  vim.cmd.startinsert()
+  local buf = vim.api.nvim_get_current_buf()
+  vim.api.nvim_create_autocmd("TermClose", {
+    buffer = buf,
+    once = true,
+    callback = function()
+      if vim.api.nvim_buf_is_valid(buf) then
+        vim.cmd.bdelete(buf)
+      end
+    end,
+  })
+end)
