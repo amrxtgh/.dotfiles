4e579b97fbb4d8e3ae8dd233e44d739eaf37f35e
amrxtgh <ghimireamrit619@gmail.com>
2026-07-17 00:58:08 +0545

    change rust config

--- nvim/lua/amrxtgh/lazy/rustacean.lua
+++ nvim/lua/amrxtgh/lazy/rustacean.lua
@@ -11,6 +11,20 @@ return {
 			},
 			server = {
 				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				on_attach = function(client, bufnr)
+					local opts = { silent = true, buffer = bufnr }
+					-- Standard Navigation
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, vim.tbl_extend('force', opts, { desc = "Go to Definition" }))
+					vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, vim.tbl_extend('force', opts, { desc = "Go to Type Definition" }))
+					-- Hover & Code Actions (Using RustLsp for specialized Rust grouping)
+					vim.keymap.set('n', 'K', '<cmd>RustLsp hover actions<CR>', vim.tbl_extend('force', opts, { desc = "Rust Hover Actions" }))
+					vim.keymap.set('n', '<leader>ca', '<cmd>RustLsp codeAction<CR>', vim.tbl_extend('force', opts, { desc = "Rust Code Actions" }))
+
+					-- Reverse Engineering & Profiling Tools
+					vim.keymap.set('n', '<leader>re', '<cmd>RustLsp expandMacro<CR>', vim.tbl_extend('force', opts, { desc = "Expand Macro Recursively" }))
+					vim.keymap.set('n', '<leader>rm', '<cmd>RustLsp viewMemoryLayout<CR>', vim.tbl_extend('force', opts, { desc = "View Structure Memory Layout" }))
+					vim.keymap.set('n', '<leader>rh', '<cmd>RustLsp toggleInlayHints<CR>', vim.tbl_extend('force', opts, { desc = "Toggle Inlay Hints" }))
+				end,
 				default_settings = {
 					["rust-analyzer"] = {
 						cargo = {
@@ -27,5 +41,12 @@ return {
 				},
 			},
 		}
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.rs",
+			callback = function()
+				vim.lsp.buf.format({ timeout_ms = 5000 })
+			end,
+		})
 	end,
 }
+
