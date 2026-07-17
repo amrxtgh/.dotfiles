fa382f545b4a8691cd65bec0cab4cb06e9bc2098
amrxtgh <ghimireamrit619@gmail.com>
2026-07-17 10:26:14 +0545

    remove bs theme and add set.lya

--- nvim/lua/amrxtgh/set.lua
+++ nvim/lua/amrxtgh/set.lua
@@ -2,8 +2,8 @@ vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.number = false
+vim.opt.relativenumber = false
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -20,7 +20,8 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.scrolloff = 8
-vim.opt.termguicolors = true
+-- vim.opt.termguicolors = true
+
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "1200"
 
