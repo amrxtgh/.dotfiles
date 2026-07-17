fd5b8cbcc22f99aab405a6bd06171c477fb07d33
amrxtgh <ghimireamrit619@gmail.com>
2026-07-17 10:26:03 +0545

    fix this bs

--- nvim/lua/amrxtgh/init.lua
+++ nvim/lua/amrxtgh/init.lua
@@ -1,6 +1,8 @@
-vim.tbl_islist = vim.tbl_islist or vim.islist
+vim.tbl_islist = vim.islist
 
 require("amrxtgh.set")
 require("amrxtgh.remap")
 require("amrxtgh.lazy_init")
+
+vim.cmd.colorscheme("elflord_custom")
 
